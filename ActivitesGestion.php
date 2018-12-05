3bc155f667b7a7609533f20a1d0965a9ff90a2a1
Killian LEPETIT <killian.lepetit@itop.fr>
2018-12-05 14:27:15 +0100

    [Suppression anciennes pages]

--- ActivitesGestion.php
+++ ActivitesGestion.php
@@ -1,6 +1,21 @@
 <?php require 'Header.php' ?>
 <script type="text/javascript" src="js/getmdl-select.js"></script>
 
+<div #Selecteur class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select">
+    <input type="text" value="" class="mdl-textfield__input" id="typeUtlisateur" readonly>
+    <input type="hidden" value="" name="typeUtlisateur">
+    <i class="mdl-icon-toggle__label material-icons">keyboard_arrow_down</i>
+    <label for="typeUtlisateur" class="mdl-textfield__label">Catégorie</label>
+    <ul for="typeUtlisateur" class="mdl-menu mdl-menu--bottom-left mdl-js-menu">
+        <li class="mdl-menu__item" data-val="All" data-selected="true">Tous</li>
+        <li class="mdl-menu__item" data-val="Admins">Admins</li>
+        <li class="mdl-menu__item" data-val="Visiteurs">Visiteurs</li>
+        <li class="mdl-menu__item" data-val="Animateurs">Animateurs</li>
+        <li class="mdl-menu__item" data-val="Désactivés">Désactivés</li>
+        <li class="mdl-menu__item" data-val="Activés">Activés</li>
+    </ul>
+</div>
+
 Activités
 
 
