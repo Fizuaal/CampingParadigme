a8e45124394f7d1f285abe258f0a8503f0557a1d
Killian LEPETIT <killian.lepetit@itop.fr>
2019-02-19 10:57:42 +0100

    Prevention bug

--- AnimCreatfct.php
+++ AnimCreatfct.php
@@ -1,4 +1,6 @@
 <?php require 'Header.php';?>
+<!-- La ligne du dessous est un test, si bug, supprimer le Meta -->
+<meta http-equiv="refresh" content="0 ; url=UsersGestion.php">
 
 <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
     <?php NewAnimation($bdd);?>
